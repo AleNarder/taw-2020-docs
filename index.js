bbf3bd7ec58a41e724e57bdbf48ca2bcfb7bdd83
Alessio Narder <alessio.narder@gmail.com>
2020-09-06 19:18:15 +0200

    Added routing index

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const port = process.env.PORT || 8282
 app.use(cors())
 app.use('/docs/backend', swaggerUI.serve, swaggerUI.setup(backendDocs));
 app.use('/docs/frontend', express.static('frontend'))
+app.route('/').get((req, res, next) => {
+  res.sendFile([__dirname, 'index.html'].join('/'))
+})
 
 app.listen(port, () => {
   console.log(`server listening on http://localhost:${port}`)
